8c0d1e67bbb146c52eaefdd5bcc730bdaa20fb94
NaokiShima <mashimashica@gmail.com>
2025-05-05 20:09:26 +0900

    change manifest

--- content/works/manifest.ts
+++ content/works/manifest.ts
@@ -6,7 +6,7 @@ export interface WorkManifestItem {
 export const worksManifest: WorkManifestItem[] = [
   { slug: "hypothesis-and-method", published: true },
   { slug: "form-of-belief", published: true }, // 1.01
-  { slug: "form-of-value", published: true }, // 1.02
+  { slug: "proof-of-value", published: true }, // 1.02
   // { slug: "dynamics-of-belief", published: true }, // 1.03
   // { slug: "universal-form-of-truth-goodness-and-beauty", published: true }, // 1.1
   // { slug: "generation-and-iteration-of-structure", published: true }, // 1.2
